48edf3ec181977014e49ba31418755cd4e092d53
Firemontaigu <jimmy.meye@yahoo.fr>
2022-09-02 12:45:28 +0200

    celui de 6h30

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,10 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World");
+		get("/hello", (req, res) -> "Hello world de la part de jimmy");
+		get("/m2i", (req, res) -> "Hello World de villeneuved'ascque");
+		get("/foot", (req, res) -> "Hello World hala madrid y nada mas");
+		get("/jimmy", (req, res) -> "Hello World de la maison");
+
 	}
 }
