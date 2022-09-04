7a9d8d7249784b6a4b7d363208fe4e2204799a69
Firemontaigu <111961359+Firemontaigu@users.noreply.github.com>
2022-09-04 19:00:35 +0200

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,10 +9,10 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello world de la part de jimmy depuis ce dimanche");
-		get("/m2i", (req, res) -> "Hello World de villeneuved'ascque de Lille ce dimanche ");
-		get("/foot", (req, res) -> "Hello World hala madrid y nada mas a hoy a hier y para siempre");
-		get("/jimmy", (req, res) -> "Hello World de la maison ce dimanche 4 septembre");
+		get("/hello", (req, res) -> "Hello world de la part de jimmy depuis ce dimanche 04 septembre 2022");
+		get("/m2i", (req, res) -> "Hello World de villeneuved'ascque de Lille ce dimanche jour de braderie");
+		get("/foot", (req, res) -> "Hello World hala madrid y nada mas a hoy a hier y para siempre let's go");
+		get("/jimmy", (req, res) -> "Hello World de la maison ce dimanche 4 septembre 2022");
 
 	}
 }
